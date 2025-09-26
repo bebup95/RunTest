21bf4e2a2329855a7484598bb1bfd4754dfc0da1
Admin <Admin@DESKTOP-UJP3L17>
2025-09-26 12:38:43 +0700

    Add project files.

---
+++ TestModule02/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestModule02")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestModule02")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d0e06970-5ab6-463d-89c2-655efb93e4c5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestModule02/UT_Lab03.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestModule02
+{
+    [TestClass]
+    public class UnitTest_TinhTienDien
+    {
+        private MethodLibrary.MethodLibrary o = new MethodLibrary.MethodLibrary();
+
+        [DataTestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+                    "|DataDirectory|\\ElectricityBillTests.csv",
+                    "ElectricityBillTests#csv",
+                    DataAccessMethod.Sequential)]
+        public void TestTinhTienDien_FromCSV()
+        {
+            // Arrange
+            int stt = Convert.ToInt32(TestContext.DataRow["STT"]);
+            int chiSoCu = Convert.ToInt32(TestContext.DataRow["ChiSoCu"]);
+            int chiSoMoi = Convert.ToInt32(TestContext.DataRow["ChiSoMoi"]);
+            double expected = Convert.ToDouble(TestContext.DataRow["Expected"]);
+
+            // Act
+            double actual = o.TinhTienDien(chiSoCu, chiSoMoi);
+
+            // Assert
+            Assert.AreEqual(expected, Math.Round(actual, 0),
+                $"Sai ở test case STT={stt} (ChiSoCu={chiSoCu}, ChiSoMoi={chiSoMoi})");
+        }
+
+        public TestContext TestContext { get; set; }
+    }
+}
