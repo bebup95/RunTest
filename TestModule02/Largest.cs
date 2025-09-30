3495090a063506cc260ab17d43b8fc4e8e9b22aa
snowyy16 <huynhson532004@gmail.com>
2025-09-30 19:12:21 +0700

    done ut_lab07

---
+++ TestModule02/Largest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestModule02
+{
+    public class LargestFinder
+    {
+        public int Largest(int[] list)
+        {
+            if (list == null)
+                throw new NullReferenceException("Array is null");
+
+            if (list.Length == 0)
+                throw new ArgumentException("Array is empty");
+
+            int num = list[0]; // Khởi tạo bằng phần tử đầu tiên
+            for (int i = 1; i < list.Length; i++)
+            {
+                if (list[i] > num)
+                {
+                    num = list[i];
+                }
+            }
+
+            return num;
+        }
+    }
+}
